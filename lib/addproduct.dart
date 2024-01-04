ed762909f46bbc0d23cf15647778f99b97eee8bc
Adham Iyad <adhamiiyad@outlook.com>
2024-01-04 18:59:20 +0200

    e1

--- lib/addproduct.dart
+++ lib/addproduct.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'main.dart';
